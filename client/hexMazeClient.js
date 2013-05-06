a31c2777f8d37ada0c9d1b0d02cc529eae96c580
Salvatore <dested@gmail.com>
2013-05-05 20:02:32 -0700

    preliminary setup junk, preliminary socketio code

---
+++ client/hexMazeClient.js
@@ -0,0 +1,47 @@
+﻿function MazeClient(ui) {
+    var self = this;
+    self.players = [];
+    self.currentPlayerID = null;
+    self.currentPlayer = null;
+    var client = io.connect('198.211.107.235:4484');
+
+
+    client.on('MazeGame.PlayerLeft', function (data) {
+
+    });
+    client.on('MazeGame.PlayerWon', function (data1) {
+        window.alert('Player ' + data1 + ' Won!');
+        client = null;
+    });
+    window.setInterval(this.$flushMoveQueue, 500);
+    var updates = [];
+    client.on('MazeGame.PlayerPositionUpdates', function (data) {
+        for (var i = 0; i < data.length; i++) {
+            updates.push(data[i]);
+        }
+    });
+    window.setInterval(function () {
+        if (updates.length > 0) {
+            var update = updates[0];
+            ui.mazeBuilders[update.id].navigate(update.navigate);
+            ui.draw();
+            updates.shift();
+        }
+    }, 75);
+    client.on('MazeGame.MazeData', function (mazeData) {
+       ui. setupGame(players, currentPlayer, mazeData);
+    });
+    client.on('MazeGame.PlayerInfo', function (data) {
+        for (var $t1 = 0; $t1 < data.length; $t1++) {
+
+            if (data[$t1].id === self.currentPlayerID) {
+                self.currentPlayer = data[$t1];
+            }
+        }
+        self.players = data;
+    });
+    client.on('MazeGame.PlayerReflect', function (data) {
+        self.currentPlayerID = data;
+    });
+
+}
---
+++ server/runner.js
@@ -0,0 +1,43 @@
+﻿setInterval(function () {
+    console.log('keep alive ' + (new Date()).toString().substr(17, 24));
+}, 10000);
+var http = require('http');
+var app = http.createServer(function (req, res) {
+    res.end();
+});
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+app.listen(4484);
+io.set('log level', 0);
+var server = new HexmazeServer();
+var count = 0;
+io.sockets.on('connection', function (socket) {
+    var userID = count++;
+    console.log('User Joined ' + userID);
+    server.addPlayer(userID, socket.emit);
+    socket.on('WaitingRoom.VoteStart', function (vote) {
+        server.changeVoteStart(userID, vote);
+    });
+    socket.on('GameRoom.PlayerMoves', function (moveData) {
+        server.movePlayer(userID, moveData);
+    });
+    socket.on('disconnect', function () {
+        console.log('User Left ' + userID);
+        server.removePlayer(userID);
+    });
+});
+
+var HexmazeServer = function () {
+    this.movePlayer = function (_userID, _moveData) { 
+    
+    };
+    this.removePlayer = function (_userID) { 
+    
+    };
+    this.changeVoteStart = function (_userID, _vote) { 
+        
+    };
+    this.addPlayer = function (_userID, _emit) { 
+    
+    };
+};
